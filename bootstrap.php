6d92d16bdf84964d246427417a2cb0134509e874
Chris <admin@chrisclarkewebservices.co.uk>
2021-01-14 10:20:07 +0000

    General tidy

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Collapsible Content Plugin
- * 
+ *
  * @package rushmark\collapsiblecontent
  * @author Chris Clarke
  * @link https://www.chrisclarkewebservices.co.uk
@@ -12,7 +12,7 @@
  * Description:       A plugin to display a teaser and, on request, a longer a version as well as an FAQ with custom post types
  * Version:           1.0
  * Author:            Chris Clarke
- * Author URI:		  https://www.chrisclarkewebservices.co.uk	
+ * Author URI:		  https://www.chrisclarkewebservices.co.uk
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       collapsible_content
  * Requires WP:		  5.6
@@ -20,19 +20,18 @@
  */
  
  namespace rushmark\collapsiblecontent;
- 
-if ( ! defined( 'ABSPATH' ) ) {
-     die( "Oh, silly, there's nothing to see here." );
+
+if (! defined('ABSPATH')) {
+    die("Oh, silly, there's nothing to see here.");
 }
 
-define( 'COLLAPSIBLE_CONTENT_PLUGIN', __FILE__ );
-define( 'COLLAPSIBLE_CONTENT_DIR', plugin_dir_path( __FILE__ ) );
-$plugin_url = plugin_dir_url( __FILE__ );
-if ( is_ssl() ) {
-     $plugin_url = str_replace( 'http://', 'https://', $plugin_url );
+define('COLLAPSIBLE_CONTENT_PLUGIN', __FILE__);
+define('COLLAPSIBLE_CONTENT_DIR', plugin_dir_path(__FILE__));
+$plugin_url = plugin_dir_url(__FILE__);
+if (is_ssl()) {
+    $plugin_url = str_replace('http://', 'https://', $plugin_url);
 }
-define( 'COLLAPSIBLE_CONTENT_URL', $plugin_url );
-define( 'COLLAPSIBLE_CONTENT_TEXT_DOMAIN', 'collapsible_content' );
+define('COLLAPSIBLE_CONTENT_URL', $plugin_url);
+define('COLLAPSIBLE_CONTENT_TEXT_DOMAIN', 'collapsible_content');
 
-include( __DIR__ . '/src/plugin.php' );
- 
+include(__DIR__ . '/src/plugin.php');
--- src/plugin.php
+++ src/plugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * Plugin Handler
-* 
+*
 * @package rushmark\collapsiblecontent;
 
 * @since 1.0.0
@@ -13,7 +13,7 @@
 
 namespace rushmark\collapsiblecontent;
 
-add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
+add_action('wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets');
 /**
  * Enqueue the plugin assets (scripts and styles).
  *
@@ -21,16 +21,17 @@ add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
  *
  * @return void
  */
-function enqueue_assets() {
-	wp_enqueue_style('dashicons');
+function enqueue_assets()
+{
+    wp_enqueue_style('dashicons');
 
-	wp_enqueue_script(
-		'collapsible-content-plugin-script',
-		COLLAPSIBLE_CONTENT_URL . 'assets/js/jquery.plugin-min.js',
-		array('jquery'),
-		'1.0.0',
-		true
-	);
+    wp_enqueue_script(
+        'collapsible-content-plugin-script',
+        COLLAPSIBLE_CONTENT_URL . 'assets/js/jquery.plugin-min.js',
+        array('jquery'),
+        '1.0.0',
+        true
+    );
 }
 
 /**
@@ -40,13 +41,14 @@ function enqueue_assets() {
  *
  * @return void
  */
-function autoload() {
-	$files = array(
-		'shortcode/shortcodes.php',
-	);
+function autoload()
+{
+    $files = array(
+        'shortcode/shortcodes.php',
+    );
 
-	foreach( $files as $file ) {
-		include( __DIR__ . '/' . $file );
-	}
+    foreach ($files as $file) {
+        include(__DIR__ . '/' . $file);
+    }
 }
 autoload();
